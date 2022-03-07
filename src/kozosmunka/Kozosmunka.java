83b3552298acf10199a579d24301d6ec09e19773
PIKSSL <purpledog120@gmail.com>
2022-03-07 12:48:23 +0100

    hibas

--- src/kozosmunka/Kozosmunka.java
+++ src/kozosmunka/Kozosmunka.java
@@ -5,6 +5,7 @@ public class Kozosmunka {
     public static void main(String[] args) {
         String nev1 = "György Krisztián";
         String nev2 = "Katona Valentin";
+        String nev3 = "asda";
     }
     
 }
