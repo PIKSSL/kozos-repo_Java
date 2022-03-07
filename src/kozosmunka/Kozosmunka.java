45f15075af89ebc1ab8fc9156667e5e516357233
dokibacsi <kvalcsi02@gmail.com>
2022-03-07 12:43:15 +0100

    2. név deklarálása

--- src/kozosmunka/Kozosmunka.java
+++ src/kozosmunka/Kozosmunka.java
@@ -4,7 +4,7 @@ package kozosmunka;
 public class Kozosmunka {
     public static void main(String[] args) {
         String nev1 = "György Krisztián";
-        String nev2;
+        String nev2 = "Katona Valentin";
     }
     
 }
